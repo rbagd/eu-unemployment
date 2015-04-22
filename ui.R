a3593717382e8d841f1180d30c6d8771ffba8099
Rytis Bagdziunas <mail@rbagd.eu>
2015-04-22 16:50:08 +0200

    Pretifying presentation.

--- ui.R
+++ ui.R
@@ -2,14 +2,22 @@ library(shiny)
 
 shinyUI(fluidPage(
   titlePanel("Faillites d'entreprises en Belgique"),
+
   sidebarLayout(
-    sidebarPanel(
-      sliderInput("month", "Date", 3, 13, 13, 1, animate=TRUE),
-      checkboxInput("capita", "Per capita", FALSE)
-    ),
-    mainPanel(
-      plotOutput("mapplot"),
-      tableOutput("summary")
+         sidebarPanel(
+              fluidRow(
+                column(9,
+                       "Disponibilité des données entre ", textOutput("firstdata", inline=TRUE),
+                       " et ", textOutput("lastdata", inline=TRUE),
+                       br(),
+                       br(),
+                       sliderInput("month", "Sélectionnez le mois dans cet intervalle: ", 1, 13, 13, 1, animate=TRUE),
+                       checkboxInput("capita", "Faillites par habitant", FALSE) ),
+                column(12,
+                       dataTableOutput("summary")
+                       ))),
+         mainPanel(
+                plotOutput("mapplot", width="100%")
     )
   )
 ))
